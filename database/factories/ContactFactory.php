d27e5dd80c4558760326b3b525b74e47e42b27cb
Nuhan-Islam <60359389+Nuhan-Islam@users.noreply.github.com>
2022-09-27 10:54:57 +0600

    27-sep-2022

--- database/factories/ContactFactory.php
+++ database/factories/ContactFactory.php
@@ -3,19 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\Company;
-
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Contact>
- */
 class ContactFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
         return [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,16 +2,13 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
+use App\Models\Contact;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+{ 
     public function run()
     {
         // \App\Models\User::factory(10)->create();
@@ -20,9 +17,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        $this->call([
-            CompaniesTableSeeder::class,
-            ContactsTableSeeder::class,
-        ]);
+        
+        // $this->call([
+        //     CompaniesTableSeeder::class,
+        //     ContactsTableSeeder::class,
+        // ]);
+
+        Company::factory()->count(10)->create();
+        Contact::factory()->count(50)->create();
     }
 }
